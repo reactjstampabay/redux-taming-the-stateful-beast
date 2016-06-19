968f7ddc8999999e17c584ee1503a4566c96a933
joshburgess <joshburgess.webdev@gmail.com>
2016-06-19 12:15:29 -0400

    Initial shell of ideas, needs more formatting

--- presentation/index.js
+++ presentation/index.js
@@ -1,5 +1,5 @@
 // Import React
-import React from 'react';
+import React, { Component } from 'react';
 import { pure } from 'recompose';
 
 // Import Spectacle Core tags
@@ -23,6 +23,9 @@ import {
   Text,
 } from 'spectacle';
 
+// Import code presentation component
+import CodeSlide from 'spectacle-code-slide';
+
 // Import image preloader util
 import preloader from 'spectacle/lib/utils/preloader';
 
@@ -37,138 +40,133 @@ require('normalize.css');
 require('spectacle/lib/themes/default/index.css');
 
 const images = {
-  city: require('../assets/city.jpg'),
-  kat: require('../assets/kat.png'),
-  logo: require('../assets/formidable-logo.svg'),
-  markdown: require('../assets/markdown.png'),
+  city: require('../assets/city.jpg').replace('/', ''),
+  kat: require('../assets/kat.png').replace('/', ''),
+  logo: require('../assets/formidable-logo.svg').replace('/', ''),
+  markdown: require('../assets/markdown.png').replace('/', ''),
+  reduxLogoTitleDark: require('../assets/redux-logo-title-dark.png').replace('/', ''),
+  reduxLogoTitleLight: require('../assets/redux-logo-title-light.png').replace('/', ''),
+  reduxLogoPNG: require('../assets/redux-logo.png').replace('/', ''),
+  reduxLogoSVG: require('../assets/redux-logo.svg').replace('/', ''),
+  wat: require('../assets/wat.png').replace('/', ''),
+  bruceLeeLimits: require('../assets/bruce-lee-limits.jpg').replace('/', ''),
+  bruceLeeAbsorb: require('../assets/bruce-lee-absorb.jpg').replace('/', ''),
 };
 
 preloader(images);
 
 const theme = createTheme({
-  primary: '#7649bb',
+  primary: '#754fb5',
+  secondary: '#FFF',
+  tertiary: '#000',
 });
 
-const Presentation = () => {
-  return (
-    <Spectacle theme={theme}>
-      <Deck transition={['zoom', 'slide']} transitionDuration={500}>
-        <Slide transition={['zoom']} bgColor='primary'>
-          <Heading size={1} fit caps lineHeight={1} textColor='black'>
-            Spectacle
-          </Heading>
-          <Heading size={1} fit caps>
-            A ReactJS Presentation Library
-          </Heading>
-          <Heading size={1} fit caps textColor='black'>
-            Where You Can Write Your Decks In JSX
-          </Heading>
-          <Link href='https://github.com/FormidableLabs/spectacle'>
-            <Text bold caps textColor='tertiary'>View on Github</Text>
-          </Link>
-          <Text textSize='1.5em' margin='20px 0px 0px' bold>Hit Your Right Arrow To Begin!</Text>
-        </Slide>
-        <Slide transition={['slide']} bgColor='black' notes='You can even put notes on your slide. How awesome is that?'>
-          <Image src={images.kat.replace('/', '')} margin='0px auto 40px' height='293px' />
-          <Heading size={2} caps fit textColor='primary' textFont='primary'>
-            Wait what?
-          </Heading>
-        </Slide>
-        <Slide transition={['zoom', 'fade']} bgColor='primary' notes='<ul><li>talk about that</li><li>and that</li></ul>'>
-          <CodePane
-            lang='jsx'
-            source={require('raw!../assets/deck.example')}
-            margin='20px auto'
-          />
-        </Slide>
-        <Slide transition={['slide']} bgImage={images.city.replace('/', '')} bgDarken={0.75}>
-          <Appear fid='1'>
-            <Heading size={1} caps fit textColor='primary'>
-              Full Width
+class Presentation extends Component {
+  render () {
+    return (
+      <Spectacle theme={theme}>
+        <Deck transition={['zoom', 'slide']} transitionDuration={500}>
+          <Slide transition={['zoom']} bgColor='tertiary'>
+            <Image src={images.reduxLogoTitleLight} margin='0 auto' height='400px' />
+            <Heading size={1} fit caps lineHeight={1} textColor='secondary' textFont='primary' margin='-50px auto 0 auto' >
+              Taming the Stateful Beast
             </Heading>
-          </Appear>
-          <Appear fid='2'>
-            <Heading size={1} caps fit textColor='tertiary'>
-              Adjustable Darkness
+          </Slide>
+          <Slide transition={['slide']} bgColor='primary'>
+            <Heading size={1} caps fit textColor='secondary' textFont='primary'>
+              "Redux is a predictable state
             </Heading>
-          </Appear>
-          <Appear fid='3'>
-            <Heading size={1} caps fit textColor='primary'>
-              Background Imagery
+            <Heading size={1} caps fit textColor='secondary' textFont='primary'>
+              container for JavaScript apps."
             </Heading>
-          </Appear>
-        </Slide>
-        <Slide transition={['zoom', 'fade']} bgColor='primary'>
-          <Heading caps fit>Flexible Layouts</Heading>
-          <Layout>
-            <Fill>
-              <Heading size={4} caps textColor='secondary' bgColor='white' margin={10}>
-                Left
+          </Slide>
+          <Slide transition={['zoom']} bgColor='tertiary'>
+            <Image src={images.wat} margin='-75px auto 0 auto' height='700px' />
+          </Slide>
+          <Slide transition={['slide']} bgColor='primary'>
+            <Heading size={1} caps fit textColor='secondary' textFont='primary'>
+              Talk about Flux here.
+            </Heading>
+          </Slide>
+          <Slide transition={['slide']} bgColor='primary'>
+            <Heading size={1} caps fit textColor='secondary' textFont='primary'>
+              Add diagrams comparing unidrectional flow vs 2-way databinding
+            </Heading>
+          </Slide>
+          <Slide transition={['slide']} bgColor='primary'>
+            <Heading size={1} caps fit textColor='secondary' textFont='primary'>
+              Talk about specific influences/inspirations here.
+            </Heading>
+            <Heading size={1} caps fit textColor='secondary' textFont='primary'>
+              Flummox, NuclearJS, & The Elm Architecture
+            </Heading>
+          </Slide>
+          <Slide transition={['slide']} bgColor='primary'>
+            <Heading size={1} caps fit textColor='secondary' textFont='primary'>
+              Go over the 3 principles of Redux
+            </Heading>
+          </Slide>
+          <Slide transition={['slide']} bgColor='primary'>
+            <Heading size={1} caps fit textColor='secondary' textFont='primary'>
+              Go through the basic pieces & workflow of Redux
+            </Heading>
+            <Heading size={1} caps fit textColor='secondary' textFont='primary'>
+              State tree, Store, Actions, Reducers, middleware, etc.
+            </Heading>
+          </Slide>
+          <Slide transition={['spin', 'slide']} bgColor='tertiary'>
+            <Heading size={1} caps fit lineHeight={1.5} textColor='primary'>
+              Recommendations
+            </Heading>
+            <List>
+              <Appear><ListItem>Learn & understand React before going all in on Redux.</ListItem></Appear>
+              <Appear><ListItem>Use & enjoy Redux. It's stable, elegant, & has a huge community.</ListItem></Appear>
+              <Appear><ListItem>...but don't stop there. Stay curious and experiment.</ListItem></Appear>
+            </List>
+          </Slide>
+          <Slide transition={['zoom']} bgColor='tertiary'>
+            <Image src={images.bruceLeeLimits} margin='0 auto' height='500px' />
+          </Slide>
+          <Slide transition={['slide']} bgColor='primary'>
+            <Heading size={1.5} caps fit textColor='secondary' textFont='primary'>
+              Pushing boundaries & rethinking best practices
+            </Heading>
+            <Heading size={1.5} caps fit textColor='secondary' textFont='primary'>
+               is what the React community is all about.
+            </Heading>
+          </Slide>
+          <Slide transition={['spin', 'slide']} bgColor='tertiary'>
+            <Heading size={1} caps fit lineHeight={1.5} textColor='primary'>
+              Going Further
+            </Heading>
+          </Slide>
+          <Slide transition={['spin', 'slide']} bgColor='tertiary'>
+            <List>
+              <ListItem>Future Talk: Getting More Functional & Immutable with React</ListItem>
+              <Appear><ListItem>Look into immutable data structures for JS: Immutable.js, Mori, etc.</ListItem></Appear>
+              <Appear><ListItem>Learn about Higher Order Components & try Recompose</ListItem></Appear>
+              <Appear><ListItem>Try more advanced middleware: redux-saga, redux-rx, etc.</ListItem></Appear>
+              <Appear><ListItem>Try state management alternatives: Mobx, reactive streams (like RxJS or most.js), etc.</ListItem></Appear>
+              <Appear><ListItem>Look to other languages & frameworks for ideas: Elm, PureScript, Cycle.js, etc.</ListItem></Appear>
+            </List>
+          </Slide>
+          <Slide transition={['zoom']} bgColor='tertiary'>
+            <Image src={images.bruceLeeAbsorb} margin='0 auto' height='500px' />
+          </Slide>
+          <Slide transition={['spin', 'slide']} bgColor='tertiary'>
+            <Heading size={1} caps fit lineHeight={1.5} textColor='primary'>
+              One More Thing...
+            </Heading>
+            <Appear>
+              <Heading size={1} caps fit lineHeight={1.5} textColor='primary'>
+                Lightning Talks!
               </Heading>
-            </Fill>
-            <Fill>
-              <Heading size={4} caps textColor='secondary' bgColor='white' margin={10}>
-                Right
-              </Heading>
-            </Fill>
-          </Layout>
-        </Slide>
-        <Slide transition={['slide']} bgColor='black'>
-          <BlockQuote>
-            <Quote>Wonderfully formatted quotes</Quote>
-            <Cite>Ken Wheeler</Cite>
-          </BlockQuote>
-        </Slide>
-        <Slide transition={['spin', 'zoom']} bgColor='tertiary'>
-          <Heading caps fit size={1} textColor='primary'>
-            Inline Markdown
-          </Heading>
-          <Markdown>
-            {`
-            ![Markdown Logo](${images.markdown.replace('/', '')})
-
-            You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-            * Lists too!
-            * With ~~strikethrough~~ and _italic_
-            * And lets not forget **bold**
-            `}
-          </Markdown>
-        </Slide>
-        <Slide transition={['slide', 'spin']} bgColor='primary'>
-          <Heading caps fit size={1} textColor='tertiary'>
-            Smooth
-          </Heading>
-          <Heading caps fit size={1} textColor='secondary'>
-            Combinable Transitions
-          </Heading>
-        </Slide>
-        <Slide transition={['fade']} bgColor='secondary' textColor='primary'>
-          <List>
-            <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-            <Appear><ListItem>Autofit text</ListItem></Appear>
-            <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-            <Appear><ListItem>React-Router navigation</ListItem></Appear>
-            <Appear><ListItem>PDF export</ListItem></Appear>
-            <Appear><ListItem>And...</ListItem></Appear>
-          </List>
-        </Slide>
-        <Slide transition={['slide']} bgColor='primary'>
-          <Heading size={1} caps fit textColor='tertiary'>
-            Your presentations are interactive
-          </Heading>
-          <Interactive />
-        </Slide>
-        <Slide transition={['spin', 'slide']} bgColor='tertiary'>
-          <Heading size={1} caps fit lineHeight={1.5} textColor='primary'>
-            Made with love in Seattle by
-          </Heading>
-          <Link href='http://www.formidablelabs.com'>
-            <Image width='100%' src={images.logo} />
-          </Link>
-        </Slide>
-      </Deck>
-    </Spectacle>
-  );
-};
+            </Appear>
+          </Slide>
+        </Deck>
+      </Spectacle>
+    );
+  }
+}
 
 export default pure(Presentation);
