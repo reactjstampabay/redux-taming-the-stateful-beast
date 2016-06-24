9ca98b6bf8148fce77290bd9314b41850aa677f8
Josh Burgess <jburgess@revenuemanage.com>
2016-06-24 14:22:08 -0400

    updates & additions

--- presentation/index.js
+++ presentation/index.js
@@ -138,12 +138,18 @@ class Presentation extends Component {
               Learning Resources
             </Heading>
             <List>
-              <ListItem>Read the official docs: <a href='http://redux.js.org/'>redux.js.org</a></ListItem>
-              <ListItem>Watch Dan Abramov's Videos on Egghead.io: <br />&nbsp;&nbsp; &nbsp;
-                <a href='https://egghead.io/courses/getting-started-with-redux'>Getting Started with Redux</a> <br />&nbsp;&nbsp; &nbsp;
-                <a href='https://egghead.io/courses/building-react-applications-with-idiomatic-redux'>Building React Applications with Idiomatic Redux</a>
-              </ListItem>
-              <ListItem>Check out <a href='https://github.com/reactjs/redux/tree/master/examples'>the examples in the redux GitHub repository</a></ListItem>
+              <Appear>
+                <ListItem>Read the official docs: <a href='http://redux.js.org/'>redux.js.org</a></ListItem>
+              </Appear>
+              <Appear>
+                <ListItem>Watch Dan Abramov's Videos on Egghead.io: <br />&nbsp;&nbsp; &nbsp;
+                  <a href='https://egghead.io/courses/getting-started-with-redux'>Getting Started with Redux</a> <br />&nbsp;&nbsp; &nbsp;
+                  <a href='https://egghead.io/courses/building-react-applications-with-idiomatic-redux'>Building React Applications with Idiomatic Redux</a>
+                </ListItem>
+              </Appear>
+              <Appear>
+                <ListItem>Check out <a href='https://github.com/reactjs/redux/tree/master/examples'>the examples in the redux GitHub repository</a></ListItem>
+              </Appear>
             </List>
           </Slide>
           <Slide transition={['spin', 'slide']} bgColor='tertiary'>
@@ -161,29 +167,56 @@ class Presentation extends Component {
           </Slide>
           <Slide transition={['slide']} bgColor='primary'>
             <Heading size={1} caps fit textColor='secondary' textFont='primary'>
-              Pushing boundaries & rethinking best practices
+              Rethinking dogma
             </Heading>
             <Heading size={1} caps fit textColor='secondary' textFont='primary'>
-               is what the React community is all about.
+              & innovating
+            </Heading>
+            <Heading size={1} caps fit textColor='secondary' textFont='primary'>
+               is what the React community
+            </Heading>
+            <Heading size={1} caps fit textColor='secondary' textFont='primary'>
+               is all about
             </Heading>
           </Slide>
           <Slide transition={['spin', 'slide']} bgColor='tertiary'>
             <Heading size={1} caps fit lineHeight={1} textColor='primary'>
-              Interested in diving deeper?
+              Interested in
+            </Heading>
+            <Heading size={1} caps fit lineHeight={1} textColor='primary'>
+              diving deeper?
             </Heading>
           </Slide>
           <Slide transition={['spin', 'slide']} bgColor='tertiary'>
             <Heading size={1} caps fit lineHeight={1} textColor='primary'>
-              Down the <span style={{ color: theme.screen.colors.secondary }}>rabbit</span> hole we go...
+              Down the
+            </Heading>
+            <Heading size={1} caps fit lineHeight={1} textColor='primary'>
+              <span style={{ color: theme.screen.colors.secondary }}>rabbit</span>
+            </Heading>
+            <Heading size={1} caps fit lineHeight={1} textColor='primary'>
+               hole we go...
             </Heading>
           </Slide>
           <Slide transition={['spin', 'slide']} bgColor='tertiary'>
+            <Heading size={1} caps fit lineHeight={1} textColor='primary'>Future Talk</Heading>
+            <Heading size={1} caps fit lineHeight={1} textColor='secondary'>Going Further with</Heading>
+            <Heading size={1} caps fit lineHeight={1} textColor='secondary'>FP & Immutability</Heading>
+            <Heading size={1} caps fit lineHeight={1} textColor='secondary'>in React & Redux</Heading>
+          </Slide>
+          <Slide transition={['spin', 'slide']} bgColor='tertiary'>
+            <Heading size={1} caps fit lineHeight={1} textColor='primary'>In the meantime...</Heading>
             <List>
-              <ListItem>Future Talk: <br />&nbsp;&nbsp; &nbsp;Going Further with FP & Immutability in React & Redux</ListItem>
-              <Appear><ListItem>Try out complimentary libraries: <br />&nbsp;&nbsp; &nbsp;<a href='https://github.com/acdlite/recompose'>recompose</a>, <a href='https://github.com/facebook/immutable-js'>immutable-js</a>, etc.</ListItem></Appear>
-              <Appear><ListItem>Test more advanced middleware: <br />&nbsp;&nbsp; &nbsp;<a href='https://github.com/yelouafi/redux-saga'>redux-saga</a>, <a href='https://github.com/redux-observable/redux-observable'>redux-observable</a>, <a href='https://github.com/acdlite/redux-rx'>redux-rx</a>, etc.</ListItem></Appear>
-              <Appear><ListItem>Consider alternatives to Redux: <br />&nbsp;&nbsp; &nbsp;<a href='https://github.com/mobxjs/mobx'>mobx</a>, reactive streams (<a href='https://github.com/ReactiveX/rxjs'>rxjs</a>, <a href='https://github.com/cujojs/most'>most</a>, <a href='https://github.com/staltz/xstream'>xstream</a>), etc.</ListItem></Appear>
-              <Appear><ListItem>Look to other frameworks & languages for ideas: <br />&nbsp;&nbsp; &nbsp;<a href='http://cycle.js.org/'>Cycle.js</a>, <a href='http://clojure.org/about/clojurescript'>ClojureScript</a> & <a href='https://github.com/omcljs/om'>Om</a>, <a href='http://elm-lang.org/'>Elm</a>, <a href='http://www.purescript.org/'>PureScript</a>, <a href='https://facebook.github.io/reason/'>Reason</a>, etc.</ListItem></Appear>
+              <ListItem>Try out complimentary libraries: <br />&nbsp;&nbsp; &nbsp;<a href='https://github.com/acdlite/recompose'>recompose</a>, <a href='https://github.com/raisemarketplace/redux-loop'>redux-loop</a>, <a href='https://github.com/reactjs/reselect'>reselect</a>, <a href='https://github.com/paularmstrong/normalizr'>normalizr</a>, <a href='https://github.com/facebook/immutable-js'>immutable-js</a></ListItem>
+              <Appear>
+                <ListItem>Explore more advanced middleware: <br />&nbsp;&nbsp; &nbsp;<a href='https://github.com/yelouafi/redux-saga'>redux-saga</a>, <a href='https://github.com/redux-observable/redux-observable'>redux-observable</a>, <a href='https://github.com/acdlite/redux-rx'>redux-rx</a></ListItem>
+              </Appear>
+              <Appear>
+                <ListItem>Consider alternatives to Redux: <br />&nbsp;&nbsp; &nbsp;<a href='https://github.com/facebook/relay'>relay</a>, <a href='https://github.com/mobxjs/mobx'>mobx</a>, reactive streams (<a href='https://github.com/ReactiveX/rxjs'>rxjs</a>, <a href='https://github.com/cujojs/most'>most</a>, <a href='https://github.com/staltz/xstream'>xstream</a>)</ListItem>
+              </Appear>
+              <Appear>
+                <ListItem>Look to other frameworks & languages for ideas: <br />&nbsp;&nbsp; &nbsp;<a href='http://cycle.js.org/'>Cycle.js</a>, <a href='http://clojure.org/about/clojurescript'>ClojureScript</a> & <a href='https://github.com/omcljs/om'>Om</a>, <a href='http://elm-lang.org/'>Elm</a>, <a href='http://www.purescript.org/'>PureScript</a>, <a href='https://facebook.github.io/reason/'>Reason</a></ListItem>
+              </Appear>
             </List>
           </Slide>
           <Slide transition={['zoom']} bgColor='tertiary'>
@@ -202,7 +235,10 @@ class Presentation extends Component {
           </Slide>
           <Slide transition={['spin', 'slide']} bgColor='tertiary'>
             <Heading size={1} caps fit lineHeight={1.5} textColor='primary'>
-              Next RehackedLab on Redux on July 6th
+              Redux Rehacked Lab
+            </Heading>
+            <Heading size={1} caps fit lineHeight={1.5} textColor='secondary'>
+              at The Hive, on July 6th
             </Heading>
           </Slide>
           <Slide transition={['spin', 'slide']} bgColor='tertiary'>
@@ -225,12 +261,12 @@ class Presentation extends Component {
           </Slide>
           <Slide transition={['spin', 'slide']} bgColor='tertiary'>
             <Heading size={1} caps fit lineHeight={1.5} textColor='primary'>
-              A few potential topic ideas, lightning or otherwise
+              Potential Topic Ideas
             </Heading>
             <List>
-              <ListItem>Data Visualization with React</ListItem>
-              <ListItem>Animation with React</ListItem>
-              <ListItem>CSS in JS</ListItem>
+              <ListItem>Working with React & Data Visualization</ListItem>
+              <ListItem>Working with React & Animation</ListItem>
+              <ListItem>An Overview of "CSS in JavaScript"</ListItem>
               <ListItem>Falcor, GraphQL, Relay, etc.</ListItem>
             </List>
           </Slide>
